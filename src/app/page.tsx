255f6bfe2decd7efe3435f765e5544c84aa39d41
Deepak Gupta <dg3667610@gmail.com>
2025-08-24 05:23:55 +0000

    Remove description of Featured projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,9 +59,6 @@ export default function Home() {
         <div className="container px-4 md:px-6 space-y-8">
             <div className="text-center">
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Featured Projects</h2>
-                <p className="mx-auto max-w-[700px] text-muted-foreground md:text-xl/relaxed">
-                    Check out some of the amazing projects hosted on our platform.
-                </p>
             </div>
           <ImageSlider />
         </div>
