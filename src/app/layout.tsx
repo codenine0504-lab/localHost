66729cd53eb117dd7296959e631b0a9700197ac0
Deepak Gupta <dg3667610@gmail.com>
2025-08-25 13:38:25 +0000

    Make localHost downlaodable as PWA
    and set localHost.png as app image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,14 +19,14 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>ð</text></svg>" />
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap"
           rel="stylesheet"
         />
-        <meta name="theme-color" content="#34D399" />
+        <meta name="theme-color" content="#38A3A5" />
       </head>
       <body className="font-body antialiased overflow-x-hidden">
          <ThemeProvider
