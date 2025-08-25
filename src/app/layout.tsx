4bf977e72e62996c3370e9595ddc14dbf565bf32
Deepak Gupta <dg3667610@gmail.com>
2025-08-25 13:42:30 +0000

    For browser favicon use green border pentagon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/localHost.png" type="image/png" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><polygon points=%2250,5 95,35 75,95 25,95 5,35%22 style=%22stroke:green;stroke-width:10;fill:none;%22/></svg>" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
