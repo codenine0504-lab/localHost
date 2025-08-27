9a14e75b8c1264db517abc878d3a05954afa1c3b
Deepak Gupta <dg3667610@gmail.com>
2025-08-27 00:32:05 +0000

    const firebaseConfig = { apiKey: "AIzaSyB6T0zAh64sKZHGm1evMS_exRgyixQQhc

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -8,15 +9,15 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB6T0zAh64sKZHGm1evMS_exRgyixQQhco",
-  authDomain: "main.firebaseapp.com",
-  projectId: "main",
-  storageBucket: "main.appspot.com",
+  authDomain: "localhost-fa8e9.firebaseapp.com",
+  projectId: "localhost-fa8e9",
+  storageBucket: "localhost-fa8e9.appspot.com",
   messagingSenderId: "72670053488",
   appId: "1:72670053488:web:ee582b2a9419c2aa8be0b5"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, "maindatabase");
+export const db = getFirestore(app, "main");
 export const auth = getAuth(app);
 export const storage = getStorage(app);
